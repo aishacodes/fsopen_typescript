d592fb59b429d9f22e40e97f321993269680cb51
Abdulfatah Aishat <aishatabdulfatah81@gmail.com>
2021-04-12 13:10:49 +0100

    9.1 Body mass index

--- calculateBmi.ts
+++ calculateBmi.ts
@@ -1,11 +1,37 @@
 const calculateBmi = (height: number, weight: number): string => {
-  const bmi:number = height / (weight * weight);
+  height=height/100
+  
+  const bmi: number = weight / (height * height);
   let bm;
-  switch (bmi) {
-    case 0 <= bmi && bmi <= 15:
+  switch (true) {
+    case 0 <= bmi && bmi < 15:
       bm = "Very severely underweight";
-      break
+      break;
+    case 15 <= bmi && bmi < 16:
+      bm = "severely underweight";
+      break;
+    case 16 <= bmi && bmi < 18.5:
+      bm = "Underweight";
+      break;
+    case 18.5 <= bmi && bmi < 25:
+      bm = "Normal (healthy weight)";
+      break;
+    case 25 <= bmi && bmi < 30:
+      bm = "Overweight";
+      break;
+    case 30 <= bmi && bmi < 35:
+      bm = "Obese Class I (Moderately obese)";
+      break;
+    case 35 <= bmi && bmi < 40:
+      bm = "Obese Class II (Severely obese)";
+      break;
+    case  bmi > 40:
+      bm = "Obese Class III (Very severely obese)";
+      break;
+    default:
+      return "INVALID DETAILS";
   }
 
   return bm;
 };
+console.log(calculateBmi(180, 74))
