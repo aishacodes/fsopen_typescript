ee3004377e24bc0c543b7eaaca8666238f2248cc
Abdulfatah Aishat <aishatabdulfatah81@gmail.com>
2021-04-14 13:35:43 +0100

    9.4 Express

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+const app = express()
+
+
+
+
+app.get("/hello", (_req, res)=>{
+    res.send("Hello Full Stack! ")
+})
+const PORT = 3030
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`)
+
+})
